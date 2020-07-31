8f4e3e1fed08bfc978c03fd9fd245fab74a42a0f
Lucas Torri <lucastorri@gmail.com>
2020-07-31 16:29:39 +0200

    Use brew resources to install jlauncher

--- Formula/jlauncher.rb
+++ Formula/jlauncher.rb
@@ -1,10 +1,25 @@
 class Jlauncher < Formula
-  url "https://github.com/programmiersportgruppe/j"
   desc "Fetches and executes jar files and their dependencies from maven"
   version "0.2.0"
-  sha256 "beafd6c15b66d7d5c2f6cf35cd21d03e1ffadde3fce407441dac9a5100127a6d"
+  homepage "https://github.com/programmiersportgruppe/j"
+  url "https://github.com/programmiersportgruppe/j"
+
+  uses_from_macos "ruby"
+
+  resource "jlauncher" do
+    url "https://rubygems.org/gems/jlauncher-0.2.0.gem"
+    sha256 "8853e67c731b82bde9cce98234284c95df8b0d1bcd02b809603a6ae0edc13ffb"
+  end
 
   def install
-    system "gem", "install", name, "--version", version, "--install-dir", prefix
+    ENV["GEM_HOME"] = libexec
+
+    resources.each do |r|
+      r.fetch
+      system "gem", "install", r.cached_download, "--no-document", "--install-dir", libexec
+    end
+
+    bin.install Dir[libexec/"bin/j"]
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 end
